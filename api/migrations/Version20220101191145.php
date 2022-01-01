3dedac110f812192ae6cf1ae7676aec6f9cd4f2e
Robin Guyomar <robin.guyomar@etu.univ-lehavre.r>
2022-01-01 20:40:23 +0100

    Coup de propre

--- api/migrations/Version20220101191145.php
+++ api/migrations/Version20220101191145.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220101175141 extends AbstractMigration
+final class Version20220101191145 extends AbstractMigration
 {
     public function getDescription(): string
     {
